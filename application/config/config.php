b19b7b899dd007b0d34096b723b1810e355a3a6c
Zongjun Lan <lanzj@elinkint.com>
2020-09-04 10:31:16 +0800

    基础config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cvsManager/';
+$config['base_url'] = 'http://127.0.0.1:8810/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/cvsManager/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
