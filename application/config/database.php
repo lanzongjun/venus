3f81208b04792ddd89645fe953b7b74a95f5332d
Zongjun Lan <lanzj@elinkint.com>
2020-09-09 09:05:06 +0800

    修改配置文件

--- application/config/database.php
+++ application/config/database.php
@@ -75,12 +75,56 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-    'hostname' => '132.232.34.181',
+    'hostname' => '127.0.0.1',
     'username' => 'root',
-    'password' => '234567',
+    'password' => '',
 	'database' => 'venus',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => 'vms_',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['mt_if'] = array(
+	'dsn'	=> '',
+    'hostname' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
+	'database' => 'iface_mt_qd',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => 'i_mt_qd_',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['ele_if'] = array(
+	'dsn'	=> '',
+    'hostname' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
+	'database' => 'iface_ele_qd',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => 'i_ele_qd_',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
