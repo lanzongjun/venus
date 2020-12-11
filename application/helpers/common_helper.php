77dbc31236ede7f41a3042211f22040b602f9a55
Zongjun Lan <lanzj@elinkint.com>
2020-12-11 09:44:29 +0800

    modify helper

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -5,7 +5,7 @@
  */
 if ( ! function_exists('my_env')){
     function my_env($env){
-        $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
+        $dotenv = Dotenv\Dotenv::createImmutable(str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
         $dotenv->load();
         return  $_ENV[$env];
     }
